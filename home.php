dd7ab868eb8b6beb6ca6b2f806993f1dcee0e959
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-17 20:05:05 +0200

    sauvegarde

--- home.php
+++ home.php
@@ -6,6 +6,21 @@
         <title>Document</title>
     </head>
     <body>
+        <?php
+            // si je suis connecté
+            if(isset($_SESSION["user"])) { ?>
+                <a href="traitement.php?action=logout">Se déconnecter</a>
+            <?php }  else { ?>
+                    <a href="traitement.php?action=login">Se connecter</a>
+                    <a href="traitement.php?action=register">S'inscrire</a>
+            <?php } ?>
+
         <h1>Accueil</h1>
+        <?php
+            if(isset($_SESSION["user"])) {
+                echo "<p>Bienvenue ".$_SESSION["user"]["pseudo"]."</p>";
+            }
+        ?>
+
     </body>
 </html>
